ad6955048be39b9ebdb6eb313761ecbc7d152433
Masoumeh Rezai <you@example.com>
2023-07-05 07:58:48 +0430

    add navgigation

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+import classes from './MainNavigation.module.css';
+
+const MainNavigation = () => {
+    return (
+        <Fragment>
+            <header>
+                <div>Great Quotes</div>
+                <nav>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </nav>
+            </header>
+        </Fragment>
+    )
+
+}
+export default MainNavigation;
