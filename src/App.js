f8e38b3db8ac906735a106174e9ce96161b3830e
Masoumeh Rezai <you@example.com>
2023-07-05 06:49:42 +0430

    add some router page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <div>
+      <Switch>
+        <Route path='/quotes'>
+
+        </Route>
+        <Route path='/quotes/:quoteId'>
+
+        </Route>
+        <Route path='/new-quote'>
+
+        </Route>
+
+      </Switch>
 
     </div>
   );
