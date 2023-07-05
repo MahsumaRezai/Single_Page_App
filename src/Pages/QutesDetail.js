791d385285c75b94decc66be9af679f57b576d70
Masoumeh Rezai <you@example.com>
2023-07-05 07:50:38 +0430

    add comment route

--- src/Pages/QutesDetail.js
+++ src/Pages/QutesDetail.js
@@ -1,12 +1,19 @@
 import { Fragment } from "react";
-import {useParams} from 'react-router-dom'
+import { useParams, Route } from 'react-router-dom';
+import Comments from '../components/comments/Comments'
+
 
 const QutesDetail = () => {
-    const params=useParams()
+    const params = useParams()
     return (
         <Fragment>
             <h2>QutesDetail page</h2>
             <p>{params.quoteId}</p>
+            <Route path={`/quotes/${params.quoteId}/comments`}>
+                <Comments />
+
+
+            </Route>
 
         </Fragment>
     )
